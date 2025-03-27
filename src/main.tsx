b91de40eb36190dac20a6f6a6867fc4114a0ad2b
Denis <denisdiogo18@hotmail.com>
2025-03-27 09:28:53 -0300

    Criando meu primeiro componente do riact com JSX (TSX)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <div>
+      <h1>Hello World! (HTML)</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+        explicabo velit? Rem accusantium odit provident laudantium magni qui
+        quam doloribus, nobis officiis laborum doloremque, corrupti deserunt
+        natus placeat praesentium iure.
+      </p>
+    </div>
   </StrictMode>,
-)
+);
