ccdb18c76ca6227c1eaba2cd51f0912ab1026e27
Denis <denisdiogo18@hotmail.com>
2025-04-02 09:54:14 -0300

    Input: usando condicionais no JSX + spred operator "...props" que busca o restante das propriedades que não foram declaradas

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' />
+            <DefaultInput id='meuInput' type='text' labelText='task' />
           </div>
           <div className='formRow'>
             <p>Lorem ipsum dolor sit amet.</p>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,11 +1,25 @@
 /*Tipagem do componente input, na primeira chave sobrescrevemos e obrigamos que o componente ao ser utilizado precisara passar obrigatóriamente o id, e junto tera as propriedadade do input atribuido  */
-type DefaultInputProps = { id: string } & React.ComponentProps<'input'>;
+type DefaultInputProps = {
+  id: string;
+  labelText: string /* Deixa o label text como opcional => labelText?: string*/;
+} & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id, type }: DefaultInputProps) {
+export function DefaultInput({
+  id,
+  type,
+  labelText,
+  ...rest
+}: DefaultInputProps) {
   return (
     <>
-      <label htmlFor={id}>task</label>
-      <input id={id} type={type} />
+      {/*
+      ****Só exibe o componente te o labelText for verdadeiro (E) se conter dados no input exemplo:
+      labelText && <label htmlFor={id}>{labelText}</label> 
+
+      **** {...rest} pega o restante das propriedades que enviar na props do componete      
+      */}
+      <label htmlFor={id}>{labelText}</label>
+      <input id={id} type={type} {...rest} />
     </>
   );
 }
