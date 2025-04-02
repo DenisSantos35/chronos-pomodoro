a916ccf8ad0cb355b1ffd89e118c1b9fb5bb4380
Denis <denisdiogo18@hotmail.com>
2025-04-02 10:29:44 -0300

    Input: adicionando estilos CSS para input ativo, em foco e desativado.

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,12 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' labelText='task' />
+            <DefaultInput
+              id='meuInput'
+              type='text'
+              labelText='task'
+              placeholder='Digite algo'
+            />
           </div>
           <div className='formRow'>
             <p>Lorem ipsum dolor sit amet.</p>
--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,3 +1,5 @@
+import styles from './styles.module.css';
+
 /*Tipagem do componente input, na primeira chave sobrescrevemos e obrigamos que o componente ao ser utilizado precisara passar obrigatóriamente o id, e junto tera as propriedadade do input atribuido  */
 type DefaultInputProps = {
   id: string;
@@ -19,7 +21,7 @@ export function DefaultInput({
       **** {...rest} pega o restante das propriedades que enviar na props do componete      
       */}
       <label htmlFor={id}>{labelText}</label>
-      <input id={id} type={type} {...rest} />
+      <input className={styles.input} id={id} type={type} {...rest} />
     </>
   );
 }
