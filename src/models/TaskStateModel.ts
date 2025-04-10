4670a1dde02c40af8c006462ed90c15d0471bc4f
Denis <denisdiogo18@hotmail.com>
2025-04-10 09:49:23 -0300

    Refatore o TaskStateModel para aprimorar os comentários e esclarecer o propósito das propriedades

--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -1,14 +1,16 @@
 import { TaskModel } from './TaskModel';
 
+// Estado -> Componente -> Filhos
+
 export type TaskStateModel = {
-  tasks: TaskModel[];
-  secondsRemaining: number; // segundos restantes
-  formatedSecondsRemaining: string; // segundos formatados
-  activeTask: TaskModel | null; // tarefa ativa
-  currentCycle: number; // ciclo atual
+  tasks: TaskModel[]; // histórico, mainform
+  secondsRemaining: number; // segundos restantes, countdown, histórico, mainform, button, home
+  formatedSecondsRemaining: string; // segundos formatados, titulo, countdown
+  activeTask: TaskModel | null; // tarefa ativa, countdown, histórico, mainform, button
+  currentCycle: number; // ciclo atual, home
   config: {
-    workTime: number; // tempo de trabalho
-    shortBreakTime: number; // tempo de pausa curta
-    longBreakTime: number; // tempo de pausa longa
+    workTime: number; // tempo de trabalho, mainform
+    shortBreakTime: number; // tempo de pausa curta, mainform
+    longBreakTime: number; // tempo de pausa longa, mainform
   };
 };
