bafa6a70c582df1b8c9cbcadb5246cd57f160431
Denis <denisdiogo18@hotmail.com>
2025-04-11 09:05:44 -0300

    Estado gobal da aplicação com useState e comunicação via props.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
+import { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplates';
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+export function Home({ state, setState }: HomeProps) {
+  console.log('Home', state);
+  console.log('Home', setState);
 
-export function Home() {
   return (
     <MainTemplate>
       <Container>
