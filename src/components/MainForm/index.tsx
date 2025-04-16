c84fa1687e6b76618804d399b676c9120bea0f8c
Denis <denisdiogo18@hotmail.com>
2025-04-16 09:26:17 -0300

    Validando formulário e criando uma nova tarefa.

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,15 +2,58 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { useRef } from 'react';
+import { useContext, useRef } from 'react';
+import { TaskModel } from '../../models/TaskModel';
+import { TaskContext } from '../../contexts/TaskContext/TaskContext';
 
 export function MainForm() {
+  const { setState } = useContext(TaskContext);
   // const [taskName, setTaskName] = useState('');
   //usando useRef para pegar o valor do input
   const taskNameInput = useRef<HTMLInputElement>(null);
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    //previnir evento de submit
     event.preventDefault();
-    console.log(`Deu certo ${taskNameInput.current?.value}`);
+
+    //pegar dados do input
+    //tratamentos para nulo e vazio.
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    //criação da nova task (tarefa)
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    //CONVERTER TEMPO EM SEGUNDOS
+    const secondsRemaining = newTask.duration * 60;
+
+    //SETAR DADOS NA TASKSTATEMODEL
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, //TODO:conferir
+        secondsRemaining, //TODO:conferir
+        formatedSecondsRemaining: '00:00', //TODO:conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+
+    console.log(taskName);
   }
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,7 @@
 //tipagem da children que vai receber o provider
 // o provider é um componente que vai envolver toda a aplicação
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TaskStateModel } from '../../models/TaskStateModel';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
@@ -13,6 +13,11 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState<TaskStateModel>(initialTaskState);
+  //verificar mudança de estado do state
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   return (
     <TaskContext.Provider value={{ state, setState }}>
       {children}
