7a34f8d905d6e72665a6c593bad1fc59cc418422
Denis <denisdiogo18@hotmail.com>
2025-04-22 09:35:23 -0300

    Como iterar elementos JSX e usar a prop key no React (do jeito certo)

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,18 +1,38 @@
+import { useContext } from 'react';
 import styles from './styles.module.css';
+import { TaskContext } from '../../contexts/TaskContext/TaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 
 export function Cycles() {
+  const { state } = useContext(TaskContext);
+  // Array.from cria um array com o tamanho do ciclo atual
+  // e preenche com valores undefined. Isso é útil para criar um array de ciclos.
+  const cycleStep = Array.from({ length: state.currentCycle });
+
+  const cycleDescriptionMap = {
+    workTime: 'foco',
+    shortBreakTime: 'descanso curto',
+    longBreakTime: 'descanso longo',
+  };
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos:</span>
       <div className={styles.cycleDots}>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.longBreakTime}`}></span>
+        {/* Sempre que utilizar map no riact, é necessário passar uma key para cada elemento */}
+        {cycleStep.map((_, index) => {
+          const nextCycle = getNextCycle(index);
+          const nextCycleType = getNextCycleType(nextCycle);
+          return (
+            <span
+              key={`${nextCycle}`}
+              className={`${styles.cycleDot} ${styles[nextCycleType]}`}
+              aria-label={`Indicador de ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+              title={`Indicador de ciclo de ${cycleDescriptionMap[nextCycleType]}`}
+            ></span>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -86,9 +86,11 @@ export function MainForm() {
       <div className='formRow'>
         <p>Próximo intervalo é de 25 min</p>
       </div>
-      <div className='formRow'>
-        <Cycles />
-      </div>
+      {state.currentCycle > 0 && (
+        <div className='formRow'>
+          <Cycles />
+        </div>
+      )}
       <div className='formRow'>
         <DefaultButton icon={<PlayCircleIcon />} color='green' />
       </div>
