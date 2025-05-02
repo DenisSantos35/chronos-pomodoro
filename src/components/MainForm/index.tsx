3369d1d530fc2f2c85359bc9688c787f89a63426
Denis <denisdiogo18@hotmail.com>
2025-05-02 09:23:03 -0300

    adicionando a data que a tarefa foi interrompida (interruptDate)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -67,7 +67,7 @@ export function MainForm() {
 
     console.log(taskName);
   }
-
+  // Interrompendo a task e zerando dados
   function handleInterruptTask() {
     setState(prevState => {
       return {
@@ -75,6 +75,12 @@ export function MainForm() {
         activeTask: null,
         secondsRemaining: 0,
         formatedSecondsRemaining: '00:00',
+        tasks: prevState.tasks.map(task => {
+          if (prevState.activeTask && prevState.activeTask.id === task.id) {
+            return { ...task, interruptDate: Date.now() };
+          }
+          return task;
+        }),
       };
     });
   }
