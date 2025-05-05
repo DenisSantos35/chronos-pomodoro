0734769960056b85484aac5426b3e810ac38ce91
Denis <denisdiogo18@hotmail.com>
2025-05-05 09:25:18 -0300

    Retornando ao código andigo TaskContextProvider após esplicação de useReducer.

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,7 @@
 //tipagem da children que vai receber o provider
 // o provider é um componente que vai envolver toda a aplicação
 
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TaskStateModel } from '../../models/TaskStateModel';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
@@ -14,63 +14,14 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState<TaskStateModel>(initialTaskState);
 
-  //Estado para tipagem da action do useReducer
-  //nossa action agora recebera 2 parametros em forma de objeto, type e payload.
-  type ActionType = {
-    type: string;
-    payload?: number;
-  };
-
-  //Reducer é uma função que recebe state e action e retorna um state
-  //Para disparar a ação do reducer é necessário ter uma função, botão ou algo que dispare.
-  const [myState, dispatch] = useReducer(
-    (state, action: ActionType) => {
-      console.log(action, state);
-
-      switch (action.type) {
-        case 'INCREMENT': {
-          if (!action.payload) return state;
-          return {
-            ...state,
-            secondsReamning: state.secondsReamning + action.payload,
-          };
-        }
-        case 'DECREMENT': {
-          if (!action.payload) return state;
-          return {
-            ...state,
-            secondsReamning: state.secondsReamning - action.payload,
-          };
-        }
-        case 'CLEAR': {
-          // if (!action.payload) return state;
-          return {
-            secondsReamning: 0,
-          };
-        }
-      }
-      return state; //estado atual, não é alterado.
-    },
-    { secondsReamning: 0 },
-  );
-  //verificar mudança de estado do state
-  // useEffect(() => {
-  //   console.log(state);
-  // }, [state]);
+  // verificar mudança de estado do state
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      <h1>O estado é: {JSON.stringify(myState)}</h1>
-      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}>
-        Incrementar + 10
-      </button>
-      <button onClick={() => dispatch({ type: 'INCREMENT', payload: 20 })}>
-        Incrementar + 20
-      </button>
-      <button onClick={() => dispatch({ type: 'DECREMENT', payload: 50 })}>
-        Decrementar - 50
-      </button>
-      <button onClick={() => dispatch({ type: 'CLEAR' })}>Clear</button>
+      {children}
     </TaskContext.Provider>
   );
 }
