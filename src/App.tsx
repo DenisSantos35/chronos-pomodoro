0932883fff8eda965ef38c9399b3094f1e9c1e66
Denis <denisdiogo18@hotmail.com>
2025-05-12 09:36:50 -0300

    React Router - Iniciando o reoteamento da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,22 @@ import { MessageContainer } from './components/MessageContainer';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { NotFound } from './pages/NotFound';
+import { AboutPomodoro } from './pages/AboutPomodoro';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessageContainer>
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </MessageContainer>
     </TaskContextProvider>
   );
