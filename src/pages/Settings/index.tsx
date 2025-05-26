a29df48201fb17f92a180ce1f67d52217fe2cc6d
Denis <denisdiogo18@hotmail.com>
2025-05-26 09:20:03 -0300

    Validando os campos do formulário antes de salvar os dados

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -6,20 +6,48 @@ import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplates';
 import { useRef } from 'react';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { showMessage } from '../../adapters/showMessage';
 
 export function Settings() {
   const { state } = useTaskContext();
+  //forma de pegar dados do input
   const workTimeInput = useRef<HTMLInputElement>(null);
   const shortBreakTimeInput = useRef<HTMLInputElement>(null);
   const longBreakTimeInput = useRef<HTMLInputElement>(null);
 
+  //validando e capturando dados do input com useRef
   function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const workTime = workTimeInput.current?.value;
-    const shortBreakTime = shortBreakTimeInput.current?.value;
-    const longBreakTime = longBreakTimeInput.current?.value;
+    showMessage.dismiss();
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
 
-    console.log(workTime, shortBreakTime, longBreakTime);
+    const formErrors = [];
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push('Por favor use apenas números para TODOS os campos');
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push('Digite valores entre 1 e 99 para foco');
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push('Digite valores entre 1 e 30 para descanso curto');
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push('Digite valores entre 1 e 60 para descanso longo');
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach(error => {
+        showMessage.error(error);
+      });
+      return;
+    }
+    console.log('SALVAR');
   }
   return (
     <MainTemplate>
@@ -41,6 +69,7 @@ export function Settings() {
               labelText='Foco'
               ref={workTimeInput}
               defaultValue={state.config.workTime}
+              type='number'
             ></DefaultInput>
           </div>
           <div className='formRow'>
@@ -49,6 +78,7 @@ export function Settings() {
               labelText='Descanso curto'
               ref={shortBreakTimeInput}
               defaultValue={state.config.shortBreakTime}
+              type='number'
             ></DefaultInput>
           </div>
           <div className='formRow'>
@@ -57,6 +87,7 @@ export function Settings() {
               labelText='Descanso longo'
               ref={longBreakTimeInput}
               defaultValue={state.config.longBreakTime}
+              type='number'
             ></DefaultInput>
           </div>
           <div className='formRow'>
